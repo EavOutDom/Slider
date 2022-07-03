b3b4936275bb07dc549b94bbfb462d9f8164c1a6
EavOutDom <eavoutdom400@gmail.com>
2022-07-03 10:34:34 +0700

    update style

--- src/app.jsx
+++ src/app.jsx
@@ -13,9 +13,11 @@ const App = () => {
     //   console.log(name);
     return (
         <div>
-            <div className="text-center mt-10 text-3xl font-bold">Sliders</div>
-            <div className="h-1 w-32 bg-white mx-auto"></div>
-            <div className="h-96 w-[36rem] bg-white mx-auto my-4 rounded-md shadow-md flex">
+            <div className="text-center mt-10 md:text-3xl text-xl duration-500 font-bold">
+                Sliders
+            </div>
+            <div className="h-1 md:w-32 w-24 duration-500 bg-white mx-auto"></div>
+            <div className="h-96 md:w-[36rem] w-80 bg-white mx-auto my-4 rounded-md shadow-md flex">
                 <Swiper
                     modules={[Navigation, Autoplay, Pagination]}
                     navigation={true}
@@ -33,7 +35,7 @@ const App = () => {
                                         src={person.image}
                                         alt={person.name}
                                         className={
-                                            "h-32 w-32 mx-auto object-cover rounded-full"
+                                            "md:h-32 md:w-32 w-20 h-20 duration-500 mx-auto object-cover rounded-full"
                                         }
                                     />
                                     <div>
